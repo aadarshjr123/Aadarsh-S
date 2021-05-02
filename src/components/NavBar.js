7a5a4960180a73708be4f58d59b5cb24e72e7527
unknown <aadarshsasikumar@gmail.com>
2021-05-02 21:53:37 +0530

    new file:   src/assets/menu.svg new file:   src/assets/x.svg modified:   src/components/NavBar.js modified:   src/styles/About.css 	modified:   src/styles/Home.css 	new file:   src/styles/Navbar.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   Nav,
   NavLink,
   Bars,
   NavMenu
 } from './NavBarElements';
+import '../styles/Navbar.css';
+import { ReactComponent as MenuIcon } from '../assets/menu.svg';
+import { ReactComponent as CloseMenu } from "../assets/x.svg";
   
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <>
-      <Nav>
-        <Bars />
-        <NavMenu>
-          <NavLink to='/about' activeStyle>
-            About
-          </NavLink>
-          <NavLink to='/Projects' activeStyle>
-            Projects
-          </NavLink>
-          <NavLink to='/Contacts' activeStyle>
-            Contacts
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </>
+    <div className="header">
+      <div className="logo-nav">
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a className="links" href="/about">About</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a className="links" href="/projects">Projects</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a className="links" href="/contacts">Contacts</a>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+    </div>
   );
 };
   
